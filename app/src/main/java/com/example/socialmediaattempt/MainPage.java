a5d46dce7cb616bd1dccc306a918f11a4438fc72
Tanner Percival <percivaltanner@gmail.com>
2019-09-12 13:55:48 -0700

    finishes phone auth

---
+++ app/src/main/java/com/example/socialmediaattempt/MainPage.java
@@ -0,0 +1,46 @@
+package com.example.socialmediaattempt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+    Button mLogout = findViewById(R.id.logout);
+    final Button mFindUser = findViewById(R.id.findUser);
+
+
+        mFindUser.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent(getApplicationContext(), FindUserActivity.class));
+
+        });
+
+
+    mLogout.setOnClickListener(new View.OnClickListener(){
+        @Override
+                public void onClick(View v) {
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+            return;
+
+        }
+
+    });
+    }
+}
